55e9077a13eb017be0877fed553e0f39893b9142
Ujjwal-patel09 <ujjupatel009@gmail.com>
2024-05-22 17:17:05 +0530

    Fixing camera position and Adding space skybox

--- Assets/scripts/camera_control.cs
+++ Assets/scripts/camera_control.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class camera_control : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public GameObject[] planet_cam_pos;
+    public GameObject sun;
+    int current_planet = 0;
+
+
+    private void Update() {
         
+        // for changing position //
+        transform.position = new Vector3(planet_cam_pos[current_planet].transform.position.x,planet_cam_pos[current_planet].transform.position.y,planet_cam_pos[current_planet].transform.position.z);
+        transform.SetParent(planet_cam_pos[current_planet].transform);//for changing rotation according to planet//
+        transform.LookAt(sun.transform.position);//for looking at sun constantly//
+
+        if(Input.GetKeyDown(KeyCode.Space)){//for switching the planet//
+            planet_switch();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void planet_switch(){
+
+        current_planet++;
+        if(current_planet == planet_cam_pos.Length){
+            current_planet = 0;
+        }
     }
 }
